a950a24a2e3a6d2c6d72550af95536f1487a380a
Shane Henry <k5blazin@gmail.com>
2018-08-14 16:29:57 -0600

    removed extra code

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "96S53Sx1Z8jM",
+    password: "XXXXXXXXXX",
     database: "bamazon"
 });
 
